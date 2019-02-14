fd86d832a8a713a016ec3d2758611083e589dd2f
Alex <alexandregiannetto@thenetgroup.net>
2019-02-14 11:50:06 +0100

    - admin Conference :   - add   - edit   - list   - delete
    Signed-off-by: Alex <alexandregiannetto@thenetgroup.net>

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Conference;
 use App\Form\ConferenceAddType;
+use App\Manager\ConferenceManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,7 +16,7 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/admin/conferences/add", name="conference_admin_add")
      */
-    public function addConference(EntityManagerInterface $entityManager,Request $request)
+    public function adminAdd(EntityManagerInterface $entityManager, Request $request)
     {
         $conference = new Conference();
         $form = $this->createForm(ConferenceAddType::class, $conference);
@@ -25,12 +26,99 @@ class ConferenceController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($conference);
             $entityManager->flush();
-            $this->redirectToRoute('conference_admin_add'); // TODO: change route to list admin
+            return $this->redirectToRoute('conferences_admin_list');
         }
 
         return $this->render('security/form.html.twig', [
             'form' => $form->createView()
         ]);
     }
+
+    /**
+     * Conference non notée
+     * @Route("/admin/conferences/list", name="conferences_admin_list")
+     */
+    public function adminList(
+        ConferenceManager $conferenceManager
+    ) {
+        $conferences = $conferenceManager->getAll();
+
+        return $this->render('conference/list.html.twig', [
+            'conferences' => $conferences,
+            'edit' => true
+        ]);
+    }
+
+    /**
+     * edit a conference
+     * @Route("/admin/conferences/edit/{id}", name="conferences_admin_edit")
+     */
+    public function adminEdit(
+        EntityManagerInterface $entityManager,
+        ConferenceManager $conferenceManager,
+        Request $request,
+        int $id
+    ) {
+        $conference = $conferenceManager->getById($id);
+        if ($conference !== null) {
+
+            $form = $this->createForm(ConferenceAddType::class, $conference);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $entityManager->persist($conference);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'The conference has been edited');
+                return $this->redirectToRoute('conferences_admin_list');
+            }
+            return $this->render('security/form.html.twig', [
+                'form' => $form->createView()
+            ]);
+
+        }
+
+        $this->addFlash('danger', 'This conference not exist.');
+        return $this->redirectToRoute('conferences_admin_list');
+    }
+
+    /**
+     * Delete a conference
+     * @Route("/admin/conferences/delete/{id}", name="conferences_admin_delete")
+     */
+    public function adminDelete(
+        EntityManagerInterface $entityManager,
+        ConferenceManager $conferenceManager,
+        int $id
+    ) {
+        $conference = $conferenceManager->getById($id);
+        if ($conference !== null) {
+
+            $entityManager->remove($conference);
+            $entityManager->flush();
+            $this->addFlash('success', 'This conference has been succefuly deleted.');
+            return $this->redirectToRoute('conferences_admin_list');
+        }
+
+        $this->addFlash('danger', 'This conference not exist.');
+        return $this->redirectToRoute('conferences_admin_list');
+    }
+
+
+    /**
+     * Conference non notée
+     * @Route("/conferences/", name="conferences_notLiked")
+     */
+    public function conferences(
+        ConferenceManager $conferenceManager
+    ) {
+        $conferences = $conferenceManager->getAll();
+
+        return $this->render('conference/list.html.twig', [
+            'conferences' => $conferences
+        ]);
+    }
+
+
 
 }
