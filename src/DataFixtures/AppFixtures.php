8f49b2055dc382df5d30d9661339e8dabf71090c
Alex <alexandregiannetto@thenetgroup.net>
2019-02-15 12:20:27 +0100

    - mise en place des fixtures pour stars
    Signed-off-by: Alex <alexandregiannetto@thenetgroup.net>

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Conference;
+use App\Entity\Stars;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -21,11 +22,15 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
 
+        $users = [];
+        $conferences = [];
+
         // Conference Fixtures
         for ($i = 0; $i <= 5; $i++) {
             $conference = new Conference();
             $conference->setName($i . ' ' . $this->getRandomWord(10));
             $conference->setDescription($this->getRandomWord());
+            $conferences[] = $conference;
             $manager->persist($conference);
 
         }
@@ -54,9 +59,18 @@ class AppFixtures extends Fixture
                 'the_new_password'
             ));
 
+            $users[] = $user;
             $manager->persist($user);
         }
 
+        for ($i = 0; $i < 10; $i++) {
+            $stars = new Stars();
+            $stars->setConference($conferences[random_int(0, count($conferences) - 1)]);
+            $stars->setUser($users[random_int(0, count($users) - 1)]);
+            $stars->setNote(random_int(1, 5));
+
+            $manager->persist($stars);
+        }
 
         $manager->flush();
     }
