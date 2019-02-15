669ca8e01d81f68dd37d76746ca0da2fc2d56233
bellatalex <bellata.alex@gmail.com>
2019-02-15 10:10:10 +0100

    ajout prenom nom

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -51,6 +51,16 @@ class User implements UserInterface
     private $nom;
 
     /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
      * User constructor.
      */
     public function __construct()
@@ -188,6 +198,30 @@ class User implements UserInterface
     public function setNom(string $nom): self
     {
         $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
 
         return $this;
     }
