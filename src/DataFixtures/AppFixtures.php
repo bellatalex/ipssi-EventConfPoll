ed3c74aa42e670d0a33f4f14d2e88e3f061f611f
Alex <alexandregiannetto@thenetgroup.net>
2019-02-15 09:33:05 +0100

    - html : ajout de la searchbar
    Signed-off-by: Alex <alexandregiannetto@thenetgroup.net>

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,9 +10,52 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        // User fixtures
+        for ($i = 0; $i <= 20; $i++) {
+            $user = new User();
+            $user->setEmail(getRandomWord(10) . '@' . getRandomWord(5) . '.com');
+
+        }
+
 
         $manager->flush();
+    }
+
+    function getRandomWord(int $maxLong = 250): string
+    {
+        $letter = [
+            'a',
+            'z',
+            'e',
+            'r',
+            't',
+            'y',
+            'u',
+            'i',
+            'o',
+            'p',
+            'q',
+            's',
+            'd',
+            'f',
+            'g',
+            'h',
+            'j',
+            'k',
+            'l',
+            'm',
+            'w',
+            'x',
+            'c',
+            'v',
+            'b',
+            'n'
+        ];
+        $str = '';
+        for ($i = 0; $i <= $maxLong; $i++) {
+            $str .= $letter[rand(0, count($letter) - 1)];
+        }
+        return $str;
     }
 }
