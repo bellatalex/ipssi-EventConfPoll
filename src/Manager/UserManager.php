7ae3f43b2cfb5283eda8696e346b82333aa0244c
bellatalex <bellata.alex@gmail.com>
2019-02-15 14:43:38 +0100

    ajout manager user

---
+++ src/Manager/UserManager.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 15/02/2019
+ * Time: 14:35
+ */
+
+namespace App\Manager;
+
+
+use App\Repository\UserRepository;
+
+class UserManager
+{
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function getAll(): ?array
+    {
+        return $this->userRepository->findAll();
+    }
+
+    public function getAllUserEmail() :?array{
+        $usersEmails = [];
+        foreach ($this->getAll() as $user){
+            $usersEmails[] = $user->getEmail();
+        }
+
+        return $usersEmails;
+    }
+
+}
